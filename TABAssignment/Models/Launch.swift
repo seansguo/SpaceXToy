6622875e57a08f3d5b2ba835735d962a34d767af
Sean Guo <guo.s.sean@gmail.com>
2019-12-07 18:21:58 +1100

    added models

---
+++ TABAssignment/Models/Launch.swift
@@ -0,0 +1,18 @@
+//
+//  Launch.swift
+//  TABAssignment
+//
+//  Created by Sean Guo on 7/12/19.
+//  Copyright © 2019 Sean Guo. All rights reserved.
+//
+
+import Foundation
+
+
+struct Launch {
+    var missionName: String
+    var launchDate: Date
+    var isSuccess: Bool
+    
+    var rocket: Rocket
+}
---
+++ TABAssignment/Models/Rocket.swift
@@ -0,0 +1,17 @@
+//
+//  Rocket.swift
+//  TABAssignment
+//
+//  Created by Sean Guo on 7/12/19.
+//  Copyright © 2019 Sean Guo. All rights reserved.
+//
+
+import Foundation
+
+struct Rocket {
+    var id: String
+    var name: String
+    var type: String
+    var description: String
+    var wikiURL: String
+}
