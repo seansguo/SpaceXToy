7480e8ee76871a4ae861a46fe2cbb049df2ee7a5
Sean Guo <guo.s.sean@gmail.com>
2019-12-07 17:44:42 +1100

    Initial Commit

---
+++ TABAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TABAssignment
+//
+//  Created by Sean Guo on 7/12/19.
+//  Copyright © 2019 Sean Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
